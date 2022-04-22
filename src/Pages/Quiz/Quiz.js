550cea155e8ff838595a7929c2a66b61b2a47e4f
Restyled.io <commits@restyled.io>
2022-04-22 15:31:14 +0000

    Restyled by clang-format

--- src/Pages/Quiz/Quiz.js
+++ src/Pages/Quiz/Quiz.js
@@ -1,27 +1,25 @@
-import { CircularProgress } from "@mui/material";
-import { useEffect, useState } from "react";
-import Question from "../../components/Question/Question";
 import "./Quiz.css";
 
-const Quiz = ({ name, questions, score, setScore, setQuestions }) => {
+import {CircularProgress} from "@mui/material";
+import {useEffect, useState} from "react";
+
+import Question from "../../components/Question/Question";
+
+const Quiz = ({name, questions, score, setScore, setQuestions}) => {
   const [options, setOptions] = useState();
   const [currQues, setCurrQues] = useState(0);
 
   useEffect(() => {
-    setOptions(
-      questions &&
-        handleShuffle([
-          questions[currQues]?.correct_answer,
-          ...questions[currQues]?.incorrect_answers,
-        ])
-    );
-  }, [currQues, questions]);
+    setOptions(questions && handleShuffle([
+                 questions[currQues]?.correct_answer,
+                 ...questions[currQues]?.incorrect_answers,
+               ]));
+  }, [ currQues, questions ]);
 
   console.log(questions);
 
-  const handleShuffle = (options) => {
-    return options.sort(() => Math.random() - 0.5);
-  };
+  const handleShuffle =
+      (options) => { return options.sort(() => Math.random() - 0.5); };
 
   return (
     <div className="quiz">
@@ -41,15 +39,18 @@ const Quiz = ({ name, questions, score, setScore, setQuestions }) => {
             correct={questions[currQues]?.correct_answer}
             score={score}
             setScore={setScore}
-            setQuestions={setQuestions}
+            setQuestions={
+    setQuestions}
           />
         </>
       ) : (
         <CircularProgress
-          style={{ margin: 100 }}
-          color="inherit"
+            style = {
+              { margin: 100 }
+            } color = "inherit"
           size={150}
-          thickness={1}
+          thickness={
+    1}
         />
       )}
     </div>
