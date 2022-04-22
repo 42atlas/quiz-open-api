2ee596f870bba01cb048610e31805859d4a78638
Restyled.io <commits@restyled.io>
2022-04-22 13:08:30 +0000

    Restyled by clang-format

--- src/Pages/Result/Result.js
+++ src/Pages/Result/Result.js
@@ -1,33 +1,37 @@
-import { Button } from '@mui/material';
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router';
 import './Result.css';
+
+import {Button} from '@mui/material';
+import {useEffect} from 'react';
+import {useNavigate} from 'react-router';
+
 import prize from '../../assets/images/omid-armin-8Nppe0yLmn8-unsplash.jpg';
 
-const Result = ({ name, score }) => {
+const Result = ({name, score}) => {
   const navigate = useNavigate();
 
   useEffect(() => {
     if (!name) {
       navigate('/');
     }
-  }, [name, navigate]);
+  }, [ name, navigate ]);
 
   return (
     <div className="result">
       <span className="title">Final Score : {score}</span>
-      {score >= 5 && <img src={prize} className="prize-picture"></img>}
-      <Button
-        variant="contained"
-        color="secondary"
-        size="large"
-        style={{ alignSelf: 'center', marginTop: 20 }}
+      {score >= 5 && <img src={prize} className="prize-picture"></img>
+} < Button
+variant = "contained"
+color = "secondary"
+size = "large"
+        style={{
+  alignSelf: 'center', marginTop: 20 }}
         href="/"
       >
         Start again
       </Button>
     </div>
   );
-};
+        }
+        ;
 
-export default Result;
+        export default Result;
