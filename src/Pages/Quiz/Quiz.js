1d21f3aec9179ae0590cf07b0a2cc74d4e7ea629
Restyled.io <commits@restyled.io>
2022-04-22 13:39:53 +0000

    Restyled by prettier

--- src/Pages/Quiz/Quiz.js
+++ src/Pages/Quiz/Quiz.js
@@ -1,25 +1,29 @@
 import "./Quiz.css";
 
-import {CircularProgress} from "@mui/material";
-import {useEffect, useState} from "react";
+import { CircularProgress } from "@mui/material";
+import { useEffect, useState } from "react";
 
 import Question from "../../components/Question/Question";
 
-const Quiz = ({name, questions, score, setScore, setQuestions}) => {
+const Quiz = ({ name, questions, score, setScore, setQuestions }) => {
   const [options, setOptions] = useState();
   const [currQues, setCurrQues] = useState(0);
 
   useEffect(() => {
-    setOptions(questions && handleShuffle([
-                 questions[currQues]?.correct_answer,
-                 ...questions[currQues]?.incorrect_answers,
-               ]));
-  }, [ currQues, questions ]);
+    setOptions(
+      questions &&
+        handleShuffle([
+          questions[currQues]?.correct_answer,
+          ...questions[currQues]?.incorrect_answers,
+        ])
+    );
+  }, [currQues, questions]);
 
   console.log(questions);
 
-  const handleShuffle =
-      (options) => { return options.sort(() => Math.random() - 1); };
+  const handleShuffle = (options) => {
+    return options.sort(() => Math.random() - 1);
+  };
 
   return (
     <div className="quiz">
@@ -39,18 +43,15 @@ const Quiz = ({name, questions, score, setScore, setQuestions}) => {
             correct={questions[currQues]?.correct_answer}
             score={score}
             setScore={setScore}
-            setQuestions={
-    setQuestions}
+            setQuestions={setQuestions}
           />
         </>
       ) : (
         <CircularProgress
-            style = {
-              { margin: 100 }
-            } color = "inherit"
+          style={{ margin: 100 }}
+          color="inherit"
           size={150}
-          thickness={
-    1}
+          thickness={1}
         />
       )}
     </div>
