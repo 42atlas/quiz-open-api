b1c95c7fad127bd7ceab4c089633817fadd023d0
Restyled.io <commits@restyled.io>
2022-04-22 13:08:33 +0000

    Restyled by prettier

--- src/Pages/Result/Result.js
+++ src/Pages/Result/Result.js
@@ -1,37 +1,37 @@
-import './Result.css';
+import "./Result.css";
 
-import {Button} from '@mui/material';
-import {useEffect} from 'react';
-import {useNavigate} from 'react-router';
+import { Button } from "@mui/material";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
 
-import prize from '../../assets/images/omid-armin-8Nppe0yLmn8-unsplash.jpg';
+import prize from "../../assets/images/omid-armin-8Nppe0yLmn8-unsplash.jpg";
 
-const Result = ({name, score}) => {
+const Result = ({ name, score }) => {
   const navigate = useNavigate();
 
   useEffect(() => {
     if (!name) {
-      navigate('/');
+      navigate("/");
     }
-  }, [ name, navigate ]);
+  }, [name, navigate]);
 
   return (
     <div className="result">
       <span className="title">Final Score : {score}</span>
-      {score >= 5 && <img src={prize} className="prize-picture"></img>
-} < Button
-variant = "contained"
-color = "secondary"
-size = "large"
+      {score >= 5 && <img src={prize} className="prize-picture"></img>}{" "}
+      <Button
+        variant="contained"
+        color="secondary"
+        size="large"
         style={{
-  alignSelf: 'center', marginTop: 20 }}
+          alignSelf: "center",
+          marginTop: 20,
+        }}
         href="/"
       >
         Start again
       </Button>
     </div>
   );
-        }
-        ;
-
-        export default Result;
+};
+export default Result;
